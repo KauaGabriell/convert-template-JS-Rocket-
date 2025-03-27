7b5e59325deec1af6d3623a06ee6c25aa36d1298
Seu Nome <seuemail@example.com>
2025-03-27 19:08:00 -0300

    Continuando Projeto

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
-const USD = 5
-const EUR = 6
-const GBP = 7
-const form = document.querySelector("form")
-const ValorDigitado = document.getElementById("amount")
-const MoedaSelecionada = document.getElementById("currency")
+const USD = 5;
+const EUR = 6;
+const GBP = 7;
+const form = document.querySelector("form");
+const ValorDigitado = document.getElementById("amount");
+const MoedaSelecionada = document.getElementById("currency");
+
 //Manipulando input para receber apenas números
-ValorDigitado.addEventListener("input", ()=>{
-    const RegexHasCharacter = /\D+/g
-    amount.value = amount.value.replace(RegexHasCharacter, "")
-})
+ValorDigitado.addEventListener("input", () => {
+  const RegexHasCharacter = /\D+/g;
+  amount.value = amount.value.replace(RegexHasCharacter, "");
+});
+
 //Capturando Moeda Selecionada
-form.onsubmit = (event) =>{
-   event.preventDefault()
-   
-   switch(convertCurrency.value){
+form.onsubmit = (event) => {
+  event.preventDefault();
+
+  switch (MoedaSelecionada.value) {
     case "USD":
-        convertCurrency(amount.value, dolar, symbol)
-   }
-   
-}
+      converCurrency(amount.value, USD, "US$");
+      break;
 
+    case "EUR":
+      converCurrency(amount.value, EUR, "Є'");
+      break
+
+    case "GBP":
+      converCurrency(amount.value, GBP, "£");
+      break
+  }
+};
 //Função para converter
-function convertCurrency(amount, price, symbol){
-
+function converCurrency(amount, price, symbol) {
+    console.log(amount, price, symbol)
 }
+
