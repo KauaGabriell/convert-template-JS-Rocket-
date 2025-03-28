f7d0d14ef9b11fc021192a9250c05f1ecc2a3687
Seu Nome <seuemail@example.com>
2025-03-28 16:44:15 -0300

    Finalizando Projeto de Estudo

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-const USD = 5;
-const EUR = 6;
-const GBP = 7;
+const USD = 5.45;
+const EUR = 6.19;
+const GBP = 7.36;
 const form = document.querySelector("form");
 const ValorDigitado = document.getElementById("amount");
 const MoedaSelecionada = document.getElementById("currency");
+const footer = document.querySelector("main footer");
+const description = document.getElementById("description");
+const result = document.getElementById("result");
 
 //Manipulando input para receber apenas números
 ValorDigitado.addEventListener("input", () => {
@@ -21,16 +24,33 @@ form.onsubmit = (event) => {
       break;
 
     case "EUR":
-      converCurrency(amount.value, EUR, "Є'");
-      break
+      converCurrency(amount.value, EUR, "Є");
+      break;
 
     case "GBP":
       converCurrency(amount.value, GBP, "£");
-      break
+      break;
   }
 };
 //Função para converter
 function converCurrency(amount, price, symbol) {
-    console.log(amount, price, symbol)
-}
+  try {
+    let total_price = amount * price;
+    total_price = formatCurrencyBRL(total_price).replace("R$", "");
+    description.textContent = `${symbol} 1 = ${formatCurrencyBRL(price)}`;
+    result.textContent = `${total_price} Reais`;
 
+    //Mostrando footer
+    footer.classList.add("show-result");
+  } catch (error) {
+    alert("Não foi possível converter. Tente Novamente mais tarde!!");
+    footer.classList.remove("show-result");
+  }
+}
+//Formata a moeda em REAL brasileiro
+function formatCurrencyBRL(value) {
+  return Number(value).toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
+}
